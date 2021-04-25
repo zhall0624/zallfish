f0d420fb4ac6888b0894bd35d2fbd5081be076fa
Zach Hall <zach@tyemill.com>
2021-04-25 02:58:01 -0700

    added a knight

--- spec/zallfish/pieces/knight_spec.rb
+++ spec/zallfish/pieces/knight_spec.rb
@@ -19,4 +19,18 @@ describe Zallfish::Pieces::Knight do
       expect(knight.moves).to match_array(expected)
     end
   end
+
+  describe "#move" do
+    it 'should move to valid square' do
+      new_square = board.squares['b'][2]
+      knight.move(new_square)
+      expect(square.piece).to be_nil
+      expect(knight.square).to eq(new_square)
+      expect(new_square.piece).to eq(knight)
+    end
+
+    it 'should not move to invalid square' do
+      expect { knight.move(board.squares['c'][4]) }.to raise_error(Zallfish::Error)
+    end
+  end
 end
